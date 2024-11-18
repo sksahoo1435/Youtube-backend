b2b00eaccada4f71b7e24cb81354563f209f07e9
sunil-sahoo-epsoft <ssahoo@eplanetsoft.com>
2024-11-18 08:37:41 +0530

    changes

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors')
+const cors = require('cors');
 const connectToDatabase = require('./middlewire/DbConnecter');
-const UserRouter = require('./routers/UserRoute')
-const ChannelRouter = require('./routers/ChannelRoute')
-const VideoRouter = require('./routers/VideoRouter')
-const CommentRouter = require('./routers/CommentRoute')
-const authToken = require('./middlewire/authMiddlewire')
+const UserRouter = require('./routers/UserRoute');
+const ChannelRouter = require('./routers/ChannelRoute');
+const VideoRouter = require('./routers/VideoRouter');
+const CommentRouter = require('./routers/CommentRoute');
+const authToken = require('./middlewire/authMiddlewire');
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 4008;
 
-
-// middlewires
+// Connect to the database
 connectToDatabase();
 
+// Middleware
 app.use(express.json());
-app.use(cors())
 
+const allowedOrigins = ['http://localhost:5173', 'https://your-frontend-domain.com','https://youtube-frontend-gray.vercel.app/'];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
 
-//Routers
+app.use('/user', UserRouter); 
+app.use('/video', VideoRouter);
+app.use('/channel', authToken, ChannelRouter);
+app.use('/comment', authToken, CommentRouter);
 
-app.use('/', UserRouter)
-app.use('/video', VideoRouter)
-app.use('/', authToken, ChannelRouter)
-app.use('/comment', authToken, CommentRouter)
-
-app.listen(process.env.PORT || 4008, () => {
-    console.log("Server started")
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
